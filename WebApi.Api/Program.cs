59ed82c1521bc6bcf8719a656e1eb8400ce759de
kuairen <kaito_127@icloud.com>
2025-08-13 14:24:26 +0000

    feat(api): :sparkles: 最低限のProgram.csを記述

--- WebApi.Api/Program.cs
+++ WebApi.Api/Program.cs
@@ -2,16 +2,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "API", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1");
+    });
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
 
 app.Run();
