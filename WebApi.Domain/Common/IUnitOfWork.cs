a22e9bca6c9a4748331388b05104d8faea760d8d
kuairen <kaito_127@icloud.com>
2025-08-16 09:58:03 +0000

    feat(infrastructure): :recycle: UnitOfWorkにリポジトリを集約

--- WebApi.Domain/Common/IUnitOfWork.cs
+++ WebApi.Domain/Common/IUnitOfWork.cs
@@ -1,6 +1,11 @@
+using WebApi.Domain.Repositories;
+
 namespace WebApi.Domain.Common;
 
 public interface IUnitOfWork
 {
+    ICustomerRepository Customers { get; }
+    IOrderRepository Orders { get; }
+
     Task<int> SaveChangeAsync(CancellationToken cancellationToken = default);
 }
--- WebApi.Infrastructure/Contexts/UnitOfWork.cs
+++ WebApi.Infrastructure/Contexts/UnitOfWork.cs
@@ -1,14 +1,20 @@
 using WebApi.Domain.Common;
+using WebApi.Domain.Repositories;
+using WebApi.Infrastructure.Repositories;
 
 namespace WebApi.Infrastructure.Contexts;
 
 public class UnitOfWork : IUnitOfWork
 {
     private readonly AppDbContext _context;
+    public ICustomerRepository Customers { get; }
+    public IOrderRepository Orders { get; }
 
     public UnitOfWork(AppDbContext context)
     {
         _context = context;
+        Customers = new CustomerRepository(_context);
+        Orders = new OrderRepository(_context);
     }
 
     public async Task<int> SaveChangeAsync(CancellationToken cancellationToken = default)
