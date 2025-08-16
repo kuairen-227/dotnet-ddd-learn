794ecc9347683fc3d5617e3bb4c1dda63604060d
kuairen <kaito_127@icloud.com>
2025-08-16 14:08:38 +0000

    test(domain): :white_check_mark: 値オブジェクト用メソッドのテスト追加

--- WebApi.Tests/UnitTests/Domain/ValueObjects/EmailTests.cs
+++ WebApi.Tests/UnitTests/Domain/ValueObjects/EmailTests.cs
@@ -36,4 +36,47 @@ public class EmailTests
         // Then
         Assert.Throws<ArgumentException>(() => new Email(invalidEmail));
     }
+
+    [Fact]
+    public void 正常系_同じ値のEmailは等しい()
+    {
+        // Given
+        var email1 = new Email("test@example.com");
+        var email2 = new Email("test@example.com");
+
+        // When
+        var areEqual = email1.Equals(email2);
+
+        // Then
+        Assert.True(areEqual);
+        Assert.Equal(email1.GetHashCode(), email2.GetHashCode());
+    }
+
+    [Fact]
+    public void 正常系_異なる値のEmailは等しくない()
+    {
+        // Given
+        var email1 = new Email("test@example.com");
+        var email2 = new Email("test2@example.com");
+
+        // When
+        var areEqual = email1.Equals(email2);
+
+        // Then
+        Assert.False(areEqual);
+        Assert.NotEqual(email1.GetHashCode(), email2.GetHashCode());
+    }
+
+    [Fact]
+    public void 正常系_値を文字列として返す()
+    {
+        // Given
+        var email = new Email("test@example.com");
+
+        // When
+        var emailString = email.ToString();
+
+        // Then
+        Assert.Equal("test@example.com", emailString);
+    }
 }
